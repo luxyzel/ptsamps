13546b4d3a64eae13330567fc13e0f71ca334cb5
luxyzel <lulu.arsolon@gmail.com>
2018-08-05 17:55:45 +0800

    procurement update

--- resources/views/admin/procurement/index.blade.php
+++ resources/views/admin/procurement/index.blade.php
@@ -1,3 +1,485 @@
 
+@include('templates.header')
+
+
+  <title>Manage Peripherals | Asset Management and Procurement System</title>
+</head>
+
+<body>
+
+	<!-- Start of Dashboard -->
+	<div class="dboard-container">
+
+		<!-- Left Side (Panel) -->
+		<div class="dboard-left-panel fl">
+			
+			<!-- Company profile -->
+			<div class="dboard-prof">
+				<img src="/img/companylogo.png" title="Project T Solutions">
+
+				<!-- Admin -->
+				<div class="dboard-admin">
+					<div class="admin-avatar fl"></div>
+					<div class="admin-profile-name fl">
+						<p>{{$admin->name}}</p>
+						<p class="admin-subtxt">Administrator</p>
+					</div>
+					<div class="clr"></div>
+				</div>
+
+			</div>
+
+			<!-- Buttons -->
+			<div class="dboard-prof">
+				<a href="{{ route('assets.deployed')}}">
+					<div class="dboard-left-but">Deployed Units</div>
+				</a>
+				<a href="{{ route('assets.stocks')}}">
+					<div class="dboard-left-but">Stock Assets</div>
+				</a>
+				<a href="{{ route('procurement.index')}}">
+					<div class="dboard-left-but">Procurement</div>
+				</a>
+				<a href="#">
+					<div class="dboard-left-but">Vendors</div>
+				</a>
+				<a href="#">
+					<div class="dboard-left-but">Calendar</div>
+				</a>
+			</div>
+		</div>
+
+		<!-- Right Side -->
+		<div class="dboard-right-panel fr">
+			
+			<!-- Menu Srip -->
+			<div class="dboard-menustrip">
+				<div class="dboard-menu-left-cont fl">
+					
+					<!-- Dashboard Upper Menus Left -->
+					<div class="dboard-left-menu fl">
+						<a href="{{route('dashboard')}}">
+							<div class="dboard-menu1-box">
+								<img src="/img/icon1.png">
+							</div>
+						</a>
+
+						<!-- popup menu name -->
+						<div id="dboard-menu-pop1">
+							<img src="/img/hoverarrow1.png">
+							<p>Dashboard</p>
+						</div>
+					</div>
+					<div class="dboard-left-menu fl">
+						<a href="{{ route('assets-management.index') }}">
+							<div class="dboard-menu3-box">
+								<img src="/img/icon3.png">
+							</div>
+						</a>
+
+						<!-- popup menu name -->
+						<div id="dboard-menu-pop3">
+							<img src="/img/hoverarrow1.png">
+							<p>Asset Management</p>
+						</div>
+					</div>
+					<div class="dboard-left-menu fl">
+						<a href="{{route('assets.track')}}">
+							<div class="dboard-menu2-box">
+								<img src="/img/icon2.png" >
+							</div>
+						</a>
+
+						<!-- popup menu name -->
+						<div id="dboard-menu-pop2">
+							<img src="/img/hoverarrow1.png">
+							<p>Asset Tracking</p>
+						</div>
+					</div>
+					<div class="dboard-left-menu fl">
+						<a href="">
+							<div class="dboard-menu4-box">
+								<img src="/img/icon4.png">
+							</div>
+						</a>
+
+						<!-- popup menu name -->
+						<div id="dboard-menu-pop4">
+							<img src="/img/hoverarrow1.png">
+							<p>P.O. Tracking</p>
+						</div>
+					</div>
+					<div class="dboard-left-menu fl">
+						<a href="{{ route('peripherals.index') }}">
+							<div class="dboard-menu5-box">
+								<img src="/img/icon5.png">
+							</div>
+						</a>
+
+						<!-- popup menu name -->
+						<div id="dboard-menu-pop5">
+							<img src="/img/hoverarrow1.png">
+							<p>Peripherals</p>
+						</div>
+					</div>
+					<div class="dboard-left-menu fl">
+						<a href="{{ route('category.index') }}">
+							<div class="dboard-menu6-box">
+								<img src="/img/icon5.png">
+							</div>
+						</a>
+
+						<!-- popup menu name -->
+						<div id="dboard-menu-pop6">
+							<img src="/img/hoverarrow1.png">
+							<p>Create Categories</p>
+						</div>
+					</div>
+					<div class="dboard-left-menu fl">
+						<a href="{{ route('users.index') }}">
+							<div class="dboard-menu7-box">
+								<img src="/img/icon5.png">
+							</div>
+						</a>
+
+						<!-- popup menu name -->
+						<div id="dboard-menu-pop7">
+							<img src="/img/hoverarrow1.png">
+							<p>Manage User</p>
+						</div>
+					</div>
+					<div class="clr"></div>
+
+				</div>
+
+				<!-- Dashboard Upper Right menus -->
+				<div class="dboard-menu-right-cont fl">
+
+					<!-- Right menus -->
+					<div class="dboard-right-menu fr">
+						<a href="#" id="acc-but">
+							<div class="dboard-rmenu1-box">
+								<img src="/img/menuicon.png">
+							</div>
+						</a>
+					</div>
+
+					<div class="dboard-right-menu fr">
+						<a href="#" id="acc-but">
+							<div class="dboard-rmenu2-box"></div>
+						</a>
+					</div>
+
+					{{-- Change UI Update to DASHBOARD MENUS --}}
+					{{-- <div class="dboard-right-menu fr" style="margin-right: 15px">
+						<a href="#" id="acc-but">
+							<div class="dboard-rmenu3-box">
+								<img src="/img/purchaseorder.png" title="Manage PO">
+							</div>
+						</a>
+					</div>
+
+					<div class="dboard-right-menu fr" style="margin-right: 5px">
+						<a href="{{ route('users.index') }}" id="acc-but">
+							<div class="dboard-rmenu4-box">
+								<img src="/img/adduser.png" title="Manage User">
+							</div>
+						</a>
+					</div> --}}
+					<div class="clr"></div>
+
+					<!--Account popup -->
+					<div id="acc-but-popup">
+						<img src="/img/hoverarrow2.png">
+						<div id="acc-but-popup-cont">
+							<a href="{{ route('acc.settings') }}">Account Settings</a><br>
+							<a href="{{route('admin.logout')}}" onclick="event.preventDefault();
+							document.getElementById('logout-form').submit(); ">Logout</a>
+						</div>
+					</div>
+
+					<form id="logout-form" action="{{route('admin.logout')}}" method="POST" style="display: none;">
+						{{ csrf_field() }}
+					</form>
+				</div>
+				<div class="clr"></div>
+			</div>
+			
+			<div class="dboard-content-menu">
+				<p><strong>VENDOR</strong></p>
+				<div class="field">
+                <label for="vendor" class="label">Vendor</label>
+                 <select name="vendor" id="vendor" class="control">
+                 
+                    <option value="SAMPLE">SAMPLE</option>
+                  
+                </select>
+              </div>
+			</div><br><br><br>
+			
+
+			<div class="manage-content">
+
+				<div class="field">
+                <label for="coname" class="label">Company Name</label>
+                <p class="control">
+                    <input type="text" class="input" name="coname" id="coname" value="">
+                </p>
+              	</div>
+
+              	<div class="field">
+                <label for="ctperson" class="label">Contact Person</label>
+                <p class="control">
+                    <input type="text" class="input" name="ctperson" id="ctperson" value="">
+                </p>
+              	</div>
+
+                <div class="field">
+                <label for="designation" class="label">Designation</label>
+                <p class="control">
+                    <input type="text" class="input" name="designation" id="designation" value="">
+                </p>
+              	</div>
+
+              	<div class="field">
+                <label for="emailadd" class="label">Email Address</label>
+                <p class="control">
+                    <input type="text" class="input" name="emailadd" id="emailadd" value="">
+                </p>
+              	</div>
+
+              	<div class="field">
+                <label for="ctnumber" class="label">Contact Number</label>
+                <p class="control">
+                    <input type="text" class="input" name="ctnumber" id="ctnumber" value="">
+                </p>
+              	</div>
+
+              	<div class="field">
+                <label for="coaddress" class="label">Company Address</label>
+                <p class="control">
+                    <input type="text" class="input" name="coaddress" id="coaddress" value="">
+                </p>
+              	</div>
+
+              	<div class="field">
+                <label for="phone" class="label">Phone</label>
+                <p class="control">
+                    <input type="text" class="input" name="phone" id="phone" value="">
+                </p>
+              	</div>
+
+              	<div class="field">
+                <label for="fax" class="label">Fax</label>
+                <p class="control">
+                    <input type="text" class="input" name="fax" id="fax" value="">
+                </p>
+              	</div>
+
+              	<div class="field">
+                <label for="vrtin" class="label">VAT Reg. TIN: </label>
+                <p class="control">
+                    <input type="text" class="input" name="vrtin" id="vrtin" value="">
+                </p>
+              	</div>
+			</div><br>
+
+			<div class="manage-content">
+				<p><strong>SHIP TO</strong></p><br>
+				
+				<div class="field">
+                <label for="coname" class="label">Company Name</label>
+                <p class="control">
+                    <input type="text" class="input" name="coname" id="coname" value="">
+                </p>
+              	</div>
+
+              	<div class="field">
+                <label for="ctperson" class="label">Contact Person</label>
+                <p class="control">
+                    <input type="text" class="input" name="ctperson" id="ctperson" value="">
+                </p>
+              	</div>
+
+                <div class="field">
+                <label for="designation" class="label">Designation</label>
+                <p class="control">
+                    <input type="text" class="input" name="designation" id="designation" value="">
+                </p>
+              	</div>
+
+              	<div class="field">
+                <label for="emailadd" class="label">Email Address</label>
+                <p class="control">
+                    <input type="text" class="input" name="emailadd" id="emailadd" value="">
+                </p>
+              	</div>
+
+              	<div class="field">
+                <label for="ctnumber" class="label">Contact Number</label>
+                <p class="control">
+                    <input type="text" class="input" name="ctnumber" id="ctnumber" value="">
+                </p>
+              	</div>
+
+              	<div class="field">
+                <label for="coaddress" class="label">Company Address</label>
+                <p class="control">
+                    <input type="text" class="input" name="coaddress" id="coaddress" value="">
+                </p>
+              	</div>
+
+              	<div class="field">
+                <label for="phone" class="label">Phone</label>
+                <p class="control">
+                    <input type="text" class="input" name="phone" id="phone" value="">
+                </p>
+              	</div>
+			</div>
+
+			<div class="manage-content">
+			<form id="order_details">
+				<table style="width: 100%; text-align: center;" id="dynamic_field">
+					<thead>
+						<tr>
+							<th>Item</th>
+							<th>Quantity</th>
+							<th>UOM</th>
+							<th>Description</th>
+							<th>Unit Price (₱)</th>
+							<th>Unit Price ($)</th>
+							<th>Total Price (₱)</th>
+							<th>Total Price ($)</th>
+							<th>Add</th>
+						</tr>
+					</thead>
+							<tr>
+								<td>
+									<div class="input_fields_wrap">
+								    <div><input type="text" name="item[]" size="45"></div>
+									</div>
+								</td>
+								<td>
+									<div class="input_fields_wrap">
+								    <div><input type="text" name="quantity[]" size="5"></div>
+									</div>
+								</td>
+								<td>
+									<div class="input_fields_wrap">
+								    <div><input type="text" name="uom[]"></div>
+									</div>
+								</td>
+								<td>
+									<div class="input_fields_wrap">
+								    <div><input type="text" name="description[]" size="40"></div>
+									</div>
+								</td>
+								<td>
+									<div class="input_fields_wrap">
+								    <div><input type="text" name="uppeso[]"></div>
+									</div>
+								</td>
+								<td>
+									<div class="input_fields_wrap">
+								    <div><input type="text" name="updollar[]"></div>
+									</div>
+								</td>
+								<td>
+									<div class="input_fields_wrap">
+								    <div><input type="text" name="tppeso[]"></div>
+									</div>
+								</td>
+								<td>
+									<div class="input_fields_wrap">
+								    <div><input type="text" name="tpdollar[]"></div>
+									</div>
+								</td>
+								<td>
+									<button type="button" name="add" id="add" class="btn btn-success">+</button>
+								</td>
+							</tr>
+					</table>
+				</form>
+			</div>
+
+			<br>
+			
+			<!-- PAGINATION -->
+
+			<!-- warning no record -->
+          	@if(Session::has('warning'))
+                <div class="comment-error">
+                   <strong><center>{{ Session::get('warning') }}</center> </strong> 
+                </div>
+            @endif
+
+		</div>
+		<div class="clr"></div>
+	</div>
+
+@include('templates.footer')
+
+
+
+<script type="text/javascript">
+    $(document).ready(function(){      
+      var postURL = "<?php echo url('addmore'); ?>";
+      var i=1;  
+
+
+      $('#add').click(function(){  
+           i++;  
+           $('#dynamic_field').append('<tr id="row'+i+'" class="dynamic-added"><td><input type="text" name="item[]" class="form-control name_list" size="45"/></td><td><input type="text" name="quantity[]" class="form-control name_list" /></td><td><input type="text" name="uom[]" class="form-control name_list" /></td><td><input type="text" name="description[]" class="form-control name_list" size="30" /></td><td><input type="text" name="uppeso[]" class="form-control name_list" /></td><td><input type="text" name="updollar[]" class="form-control name_list" /></td><td><input type="text" name="tppeso[]" class="form-control name_list" /></td><td><input type="text" name="tpdollar[]" class="form-control name_list" /></td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');  
+      });  
+
+
+      $(document).on('click', '.btn_remove', function(){  
+           var button_id = $(this).attr("id");   
+           $('#row'+button_id+'').remove();  
+      });  
+
+
+      $.ajaxSetup({
+          headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          }
+      });
+
+
+      $('#submit').click(function(){            
+           $.ajax({  
+                url:postURL,  
+                method:"POST",  
+                data:$('#add_name').serialize(),
+                type:'json',
+                success:function(data)  
+                {
+                    if(data.error){
+                        printErrorMsg(data.error);
+                    }else{
+                        i=1;
+                        $('.dynamic-added').remove();
+                        $('#add_name')[0].reset();
+                        $(".print-success-msg").find("ul").html('');
+                        $(".print-success-msg").css('display','block');
+                        $(".print-error-msg").css('display','none');
+                        $(".print-success-msg").find("ul").append('<li>Record Inserted Successfully.</li>');
+                    }
+                }  
+           });  
+      });  
+
+
+      function printErrorMsg (msg) {
+         $(".print-error-msg").find("ul").html('');
+         $(".print-error-msg").css('display','block');
+         $(".print-success-msg").css('display','none');
+         $.each( msg, function( key, value ) {
+            $(".print-error-msg").find("ul").append('<li>'+value+'</li>');
+         });
+      }
+    });  
+</script>
+
 
 			
