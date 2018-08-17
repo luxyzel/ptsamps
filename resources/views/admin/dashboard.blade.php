69fe475c9c6efa4df2325ecf038c960d3b49f6d9
gemscal <jamescarljunio@gmail.com>
2018-08-18 03:36:06 +0800

    another ui update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -207,9 +207,34 @@
 					</form>
 				</div>
 				<div class="clr"></div>
-
-				
 			</div>
+
+            {{-- First set of contents dashboard  --}}
+            <div class="app-dboard-cont" style="margin-top: 30px">
+                <div class="app-cont-box2 fl">
+                    <p class="app-cont-title-blue">Purchase Order Summary</p>
+
+                    {{--  --}}
+                </div>
+                <div class="app-cont-box1 fl">
+                    <p class="app-cont-title-green">Monthly P.O. Cost</p>
+                </div>
+                <div class="clr"></div>
+            </div>
+
+            {{-- First set of contents dashboard  --}}
+            <div class="app-dboard-cont" style="margin-top: 20px">
+                <div class="app-cont-box1 fl" style="margin-right: 25px; width: 32%;">
+                    <p class="app-cont-title-orange">Total Asset and Peripherals</p>
+                </div>
+                <div class="app-cont-box1 fl" style="margin-right: 20px; width: 31%;">
+                    <p class="app-cont-title-violet">Number of user</p>
+                </div>
+                <div class="app-cont-box1 fl" style="width: 33%;">
+                    <p class="app-cont-title-pink">Graph</p>
+                </div>
+                <div class="clr"></div>
+            </div>
 
 		</div>
 		<div class="clr"></div>
